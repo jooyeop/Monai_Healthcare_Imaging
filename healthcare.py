c1daa2ced839c53b54a206970184e1d11ff4a63e
jooyeop <royalmansu@gmail.com>
2023-01-04 13:37:51 +0900

    Healthcare

--- healthcare.py
+++ healthcare.py
@@ -1,1 +1,56 @@
+# 머신러닝 및 데이터 처리 관련 라이브러리
+import tensorflow as tf
+import tensorflow_hub as tfhub
+import tensorflow_addons as tfa
+import pandas as pd
+import numpy as pd
+from sklearn.preprocessing import RobustScaler
+from pandarallel import pandarallel ; pandarallel.initialize()
+from sklearn.model_selection import GroupKFold, StratifiedKFold
+
+# Built in 라이브러리
+from kaggledatasets import KaggleDatasets
+from collections import Counter
+from datetime import datetime
+from glob import glob
+import warnings
+import requests
+import hashlib
+import imageio
+import IPython
+import sklearn
+import urllib
+import zipfile
+import pickle
+import random
+import shutil
+import string
+import json
+import math
+import time
+import gzip
+import ast
+import sys
+import io
 import os
+import gc
+import re
+
+# 시각화 관련 라이브러리
+from matplotlib.colors import ListedColormap
+from matplotlib.patches import Rectangle
+import matplotlib.patches as patches
+import plotly.graph_objects as go
+import matplotlib.pyplot as plt
+from tqdm.notebook import tqdm; tqdm.pandas();
+import plotly.express as px
+import seaborn as sns
+from PIL import Image, ImageEnhance
+import matplotlib
+from matplotlib import animation, rc; rc('animation', html='jshtml')
+import plotly
+import PIL
+import cv2
+
+import plotly.io as pio
+print(pio.renderers)
